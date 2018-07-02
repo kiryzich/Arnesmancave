06e21ab6277506153fbe645227e314daa7a10c3c
1 <a1@Ugurs-MacBook-Pro.local>
2018-07-02 11:19:40 +0200

    eerste commnit

---
+++ Barhulp.java
@@ -0,0 +1,20 @@
+class Barhulp{
+        public static void main(String[] args){
+                System.out.println("Dit is de barhulp");
+                int Leeftijd = 19;
+            System.out.println("Deze persoon is 19");
+                    if(Leeftijd < 16){
+                        System.out.println("EN is veel te jong om te zuipen");
+                    } 
+                    else if(Leeftijd == 16){
+                        System.out.println("EN mag bier OF wijn");
+                    } 
+                    else if(Leeftijd == 17) {
+                        System.out.println("EN mag bier OF wijn");
+                    }
+                    else if(Leeftijd > 17) {
+                        System.out.println("EN mag alle drank");
+                    }
+        }
+}
+    
